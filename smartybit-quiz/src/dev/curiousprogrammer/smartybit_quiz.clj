973dc4e964d860dbadc59141aba0703259b4f168
Clarice Bouwer <cbillowes@gmail.com>
2024-12-31 03:38:57 +0400

    Initial commit 🦄

---
+++ smartybit-quiz/src/dev/curiousprogrammer/smartybit_quiz.clj
@@ -0,0 +1,12 @@
+(ns dev.curiousprogrammer.smartybit-quiz
+  (:gen-class))
+
+(defn greet
+  "Callable entry point to the application."
+  [data]
+  (println (str "Hello, " (or (:name data) "World") "!")))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (greet {:name (first args)}))
