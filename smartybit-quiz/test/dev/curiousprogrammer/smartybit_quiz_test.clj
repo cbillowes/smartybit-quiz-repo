9976f8ad2d437679d894eb388f4ad54d27131891
Clarice Bouwer <cbillowes@gmail.com>
2024-12-31 04:09:26 +0400

    chore: Cleanup unnecessary directories & files
    This is in favor of our new structure using bases, components, development and projects directories.

--- smartybit-quiz/test/dev/curiousprogrammer/smartybit_quiz_test.clj
+++
@@ -1,7 +0,0 @@
-(ns dev.curiousprogrammer.smartybit-quiz-test
-  (:require [clojure.test :refer :all]
-            [dev.curiousprogrammer.smartybit-quiz :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 0))))
