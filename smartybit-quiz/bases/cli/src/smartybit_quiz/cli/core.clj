6766f309b363fea9d0adc3a7a22ac2b87c391c68
Clarice Bouwer <cbillowes@gmail.com>
2024-12-31 04:40:45 +0400

    chore: Configure CLI base
    cli core
    
    chore: cli base
    
    chore: update base cli

---
+++ smartybit-quiz/bases/cli/src/smartybit_quiz/cli/core.clj
@@ -0,0 +1,4 @@
+(ns smartybit-quiz.cli.core)
+
+(defn -main [& args]
+  (println "Hello, World!"))
---
+++ smartybit-quiz/bases/cli/test/smartybit_quiz/cli/core_test.clj
@@ -0,0 +1,6 @@
+(ns smartybit-quiz.cli.core-test
+  (:require [clojure.test :as test :refer :all]
+            [smartybit-quiz.cli.core :as core]))
+
+(deftest dummy-test
+  (is (= 1 1)))
