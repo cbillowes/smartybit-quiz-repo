d1983457b1e5f77e86c2886b003cfa6a5c9a68e2
Clarice Bouwer <cbillowes@gmail.com>
2025-01-07 13:25:57 +0400

    chore: Add missing deps and mount start on webapp.core

--- backend/bases/webapp/src/smartybit/webapp/core.clj
+++ backend/bases/webapp/src/smartybit/webapp/core.clj
@@ -1,9 +1,10 @@
 (ns smartybit.webapp.core
   (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]))
+            [io.pedestal.http.route :as route]
+            [mount.core :as mount]
+            [smartybit.profile.repository.mongodb :as repository]))
 
 (defn respond-hello [request]
-  (println request)
   {:status 200 :body "Hello, world!"})
 
 
@@ -13,6 +14,7 @@
 
 
 (defn start []
+  (mount/start #'repository/conn)
   (-> {::http/routes routes
        ::http/port 8890
        ::http/type :jetty}
