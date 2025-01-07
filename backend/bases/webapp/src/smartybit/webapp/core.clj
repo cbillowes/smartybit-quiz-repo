5d41bc394788e32fc937e0e7a8909540256def8d
Clarice Bouwer <cbillowes@gmail.com>
2025-01-07 11:10:56 +0400

    chore: (webapp) Create skeleton webserver using pedestal

--- backend/bases/webapp/src/smartybit/webapp/core.clj
+++ backend/bases/webapp/src/smartybit/webapp/core.clj
@@ -1,1 +1,21 @@
-(ns smartybit.webapp.core)
+(ns smartybit.webapp.core
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
+
+(defn respond-hello [request]
+  (println request)
+  {:status 200 :body "Hello, world!"})
+
+
+(def routes
+  (route/expand-routes
+   #{["/greet" :get respond-hello :route-name :greet]}))
+
+
+(defn start []
+  (-> {::http/routes routes
+       ::http/port 8890
+       ::http/type :jetty}
+      (http/create-server)
+      (http/start)))
+
