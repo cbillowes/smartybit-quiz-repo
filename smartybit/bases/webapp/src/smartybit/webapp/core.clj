8319a9d7a973408518aff51d4899c69a01856346
Clarice Bouwer <cbillowes@gmail.com>
2025-01-07 10:37:28 +0400

    feat: Create the webapp base for BFF

---
+++ smartybit/bases/webapp/src/smartybit/webapp/core.clj
@@ -0,0 +1,1 @@
+(ns smartybit.webapp.core)
---
+++ smartybit/bases/webapp/test/smartybit/webapp/core_test.clj
@@ -0,0 +1,6 @@
+(ns smartybit.webapp.core-test
+  (:require [clojure.test :as test :refer :all]
+            [smartybit.webapp.core :as core]))
+
+(deftest dummy-test
+  (is (= 1 1)))
